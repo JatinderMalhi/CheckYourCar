1f4a7aa168cd1464c9a1f4b009b623f4daff8884
Jatinder Singh Malhi <88786451+JatinderMalhi@users.noreply.github.com>
2021-10-04 23:50:51 +1100

    completed

---
+++ CheckYourCar/Models/CarModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBusTransportation.Models
+{
+    public class CarModel
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+
+        public int ID { get; set; }
+       
+        public int CompanyID { get; set; }
+
+        public string Model { get; set; }
+
+    }
+}
---
+++ CheckYourCar/Models/CarsRecalls.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBusTransportation.Models
+{
+    public class CarsRecall
+    {
+        [Key]
+        public int ID { get; set; }
+        public int CompanyID { get; set; }
+        public int ModelID { get; set; }
+        public DateTime RecallDate { get; set; }
+        public string Issues { get; set; }
+
+
+
+    }
+}
